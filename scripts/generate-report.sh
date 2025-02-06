4734e6610294996d99f5febe7c5009528a90efb7
merkleplant <pascal@merkleplant.xyz>
2025-02-06 21:54:37 +0100

    website: clean

--- scripts/generate-report.sh
+++ scripts/generate-report.sh
@@ -5,7 +5,6 @@
 rpcs=(
     "https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"          #     1 - Ethereum
     "https://opt-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"          #    10 - Optimism
-    "https://zksync-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"       #   324 - zkSync
     "https://polygonzkevm-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}" #  1101 - Polygon zkEVM
     "https://arb-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"          # 42161 - Arbitrum One
     "https://arbnova-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"      # 42170 - Arbitrum Nova
