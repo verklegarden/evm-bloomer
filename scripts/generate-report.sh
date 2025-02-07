51e4b87fe25ef608a01ed380133d9890da262d4b
merkleplant <pascal@merkleplant.xyz>
2025-02-07 23:34:21 +0100

    add more chains

--- scripts/generate-report.sh
+++ scripts/generate-report.sh
@@ -5,6 +5,8 @@
 rpcs=(
     "https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"          #     1 - Ethereum
     "https://opt-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"          #    10 - Optimism
+    "https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"      #   137 - Polygon PoS
+    "https://worldchain-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"   #   480 - World Chain
     "https://polygonzkevm-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}" #  1101 - Polygon zkEVM
     "https://arb-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"          # 42161 - Arbitrum One
     "https://arbnova-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}"      # 42170 - Arbitrum Nova
